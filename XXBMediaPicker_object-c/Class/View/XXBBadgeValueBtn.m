f230351a24f1ee60ef1064df8853f1adbd941e85
xiaobing <xiaobing3@staff.sina.com.cn>
2016-05-26 15:14:18 +0800

    优化了cell 选中的时候的卡顿问题

--- XXBMediaPicker_object-c/Class/View/XXBBadgeValueBtn.m
+++ XXBMediaPicker_object-c/Class/View/XXBBadgeValueBtn.m
@@ -27,8 +27,9 @@
     self.hidden = YES;
     self.userInteractionEnabled = NO;
     self.titleLabel.font = [UIFont systemFontOfSize:14];
-    self.backgroundColor = [UIColor orangeColor];
     self.clipsToBounds = YES;
+    UIImage *image = [UIImage imageNamed:@"XXBPageNumber"];
+    [self setBackgroundImage:[image stretchableImageWithLeftCapWidth:image.size.width * 0.5 topCapHeight:image.size.height * 0.5] forState:UIControlStateNormal];
 }
 
 - (void)setBadgeValue:(NSInteger)badgeValue {
@@ -37,7 +38,6 @@
     }
     _badgeValue = badgeValue;
     if (_badgeValue > 0) {
-        
         NSString *bageString = [NSString stringWithFormat:@"%@",@(_badgeValue)];
         /**
          *  有值并且值不等于1 的情况向才进行相关设置
@@ -59,31 +59,32 @@
 
 - (void)p_setupFrame {
     // 根据文字的多少动态计算frame
-    self.tag = self.badgeValue ;
-    dispatch_async(dispatch_get_global_queue(0, 0), ^{
-        NSDictionary *attrs = @{NSFontAttributeName : self.titleLabel.font};
-        CGSize textSize =[self.titleLabel.text boundingRectWithSize:CGSizeMake(300, 300) options:NSStringDrawingUsesLineFragmentOrigin attributes:attrs context:nil].size;
-        CGRect frame = CGRectMake(5, 5, textSize.width, textSize.height);
-        CGFloat badgeH = self.titleLabel.frame.size.height > 0 ? self.titleLabel.frame.size.height : 17;
-        badgeH += 4;
-        CGFloat length = 0.6;
-        NSInteger temp = _badgeValue;
-        if (temp == maxBadgeValue) {
-            temp++;
-        }
-        while (temp) {
-            length += 0.4;
-            temp /= 10;
-        }
-        frame.size.height = badgeH;
-        frame.size.width = length * badgeH;
-        dispatch_async(dispatch_get_main_queue(), ^{
-            if (self.tag == self.badgeValue) {
-                self.layer.cornerRadius = badgeH * 0.5;
-                self.frame = frame;
-            }
-        });
-    });
+    //FIXME: fix some bug make app run quickly
+    // 解决的大量计算带来的卡顿问题
+    if (self.badgeValue < 10) {
+        self.frame = CGRectMake(5, 5, 20, 20);
+    } else if (self.badgeValue < 100) {
+        self.frame = CGRectMake(5, 5, 28, 20);
+    } else {
+        self.frame = CGRectMake(5, 5, 32, 20);
+    }
+    
+    //    NSDictionary *attrs = @{NSFontAttributeName : self.titleLabel.font};
+    //    CGSize textSize =[self.titleLabel.text boundingRectWithSize:CGSizeMake(300, 300) options:NSStringDrawingUsesLineFragmentOrigin attributes:attrs context:nil].size;
+    //    CGRect frame = CGRectMake(5, 5, textSize.width, textSize.height);
+    //    CGFloat badgeH = 20;
+    //    CGFloat length = 0.6;
+    //    NSInteger temp = _badgeValue;
+    //    if (temp == maxBadgeValue) {
+    //        temp++;
+    //    }
+    //    while (temp) {
+    //        length += 0.4;
+    //        temp /= 10;
+    //    }
+    //    frame.size.height = badgeH;
+    //    frame.size.width = length * badgeH;
+    //    self.frame = frame;
 }
 
 @end
